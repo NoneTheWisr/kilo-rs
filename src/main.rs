d808ede83fc8faff6953e5e69cd59f706a3179dd
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-03-17 16:40:41 +0400

    remove Results; add read_byte

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,28 @@
 use std::io::{self, Read};
+use std::slice;
 use termion::raw::IntoRawMode;
 
-type Result<T> = core::result::Result<T, Error>;
-
-#[derive(Debug)]
-enum Error {
-    IoError(io::Error),
-    KiloError(String),
+fn read_byte(reader: &mut impl Read) -> io::Result<u8> {
+    let mut byte: u8 = 0;
+    reader.read(slice::from_mut(&mut byte)).map(|_| byte)
 }
 
-impl From<io::Error> for Error {
-    fn from(error: io::Error) -> Self {
-        Self::IoError(error)
-    }
-}
-
-fn main() -> Result<()> {
+fn main() {
     let _stdout = io::stdout().into_raw_mode().unwrap();
+    let mut stdin = io::stdin();
 
     loop {
-        let mut b: u8 = 0;
-        io::stdin().read(std::slice::from_mut(&mut b))?;
-
-        let b = b;
+        let b = read_byte(&mut stdin).unwrap();
         let c = b as char;
+
         if c.is_control() {
             print!("{:?}\r\n", b);
         } else {
             print!("{:?} ({})\r\n", b, c);
         }
+
         if c == 'q' {
             break;
         }
     }
-    
-    Ok(())
 }
