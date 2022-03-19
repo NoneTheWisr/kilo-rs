a25a325383348871d5fbe21ffdbd1f27e5ce01b8
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-03-19 15:03:24 +0400

    implement chapter 3 from the tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,160 @@
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
-use crossterm::{event, terminal};
-use std::time::Duration;
 use anyhow::Result;
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::event::{self, Event, KeyEvent};
+use crossterm::style::Print;
+use crossterm::terminal::{self, Clear, ClearType::*};
+use crossterm::queue;
+use std::io::{self, BufWriter, Stdout, Write};
+
+pub struct Kilo {
+    cursor: Cursor,
+    dimentions: Dimentions,
+    is_running: bool,
+    stdout: BufWriter<Stdout>,
+}
+
+impl Kilo {
+    pub fn new() -> Result<Self> {
+        Ok(Self {
+            cursor: Cursor::new(0, 0),
+            dimentions: Dimentions::from(terminal::size()?),
+            is_running: true,
+            stdout: BufWriter::new(io::stdout()),
+        })
+    }
+
+    pub fn run(&mut self) -> Result<()> {
+        let _override = RawModeOverride::new()?;
+
+        while self.is_running {
+            self.render()?;
+            self.process_input()?;
+        }
+
+        Ok(())
+    }
+
+    fn terminate(&mut self) -> Result<()> {
+        self.is_running = false;
+        queue!(self.stdout, Clear(All), MoveTo(0, 0))?;
+        Ok(())
+    }
+
+    fn render(&mut self) -> Result<()> {
+        queue!(self.stdout, Hide)?;
+        queue!(self.stdout, MoveTo(0, 0))?;
+
+        self.render_rows()?;
+
+        queue!(self.stdout, Show)?;
+        queue!(self.stdout, MoveTo(self.cursor.x, self.cursor.y))?;
+
+        self.stdout.flush()?;
+        Ok(())
+    }
+
+    fn render_rows(&mut self) -> Result<()> {
+        for row in 0..self.dimentions.height {
+            if row == self.dimentions.height / 3 {
+                let message_row = self.make_message_row();
+                queue!(self.stdout, Print(message_row))?;
+            } else {
+                queue!(self.stdout, Print("~"))?;
+            }
+
+            queue!(self.stdout, Clear(UntilNewLine))?;
+            if row < self.dimentions.height - 1 {
+                queue!(self.stdout, Print("\r\n"))?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn make_message_row(&self) -> String {
+        let message = "kilo-rs (week 2)";
+        let width_bound = std::cmp::min(message.len(), self.dimentions.width as usize);
+        let message = &message[..width_bound];
+
+        let padding_len = (self.dimentions.width as usize - message.len()) / 2;
+        if padding_len > 0 {
+            format!("~{}{message}", " ".repeat(padding_len - 1))
+        } else {
+            String::from(message)
+        }
+    }
+
+    #[rustfmt::skip]
+    fn process_input(&mut self) -> Result<()> {
+        if let Event::Key(KeyEvent { code, modifiers }) = event::read()? {
+            use event::KeyCode::*;
+            use event::KeyModifiers as KM;
+
+            match (modifiers, code) {
+                (KM::NONE, Up)       => self.move_cursor_up(),
+                (KM::NONE, Down)     => self.move_cursor_down(),
+                (KM::NONE, Left)     => self.move_cursor_left(),
+                (KM::NONE, Right)    => self.move_cursor_right(),
+
+                (KM::NONE, Home)     => self.move_cursor_to_line_start(),
+                (KM::NONE, End)      => self.move_cursor_to_line_end(),
+
+                (KM::NONE, PageUp)   => self.move_cursor_to_screen_top(),
+                (KM::NONE, PageDown) => self.move_cursor_to_screen_bottom(),
+
+                (KM::CONTROL, Char('q')) => self.terminate()?,
+
+                _ => {},
+            }
+        }
+
+        Ok(())
+    }
+
+    fn move_cursor_up(&mut self) {
+        if self.cursor.y > 0 {
+            self.cursor.y -= 1;
+        }
+    }
+
+    fn move_cursor_down(&mut self) {
+        if self.cursor.y + 1 < self.dimentions.height {
+            self.cursor.y += 1;
+        }
+    }
+
+    fn move_cursor_left(&mut self) {
+        if self.cursor.x > 0 {
+            self.cursor.x -= 1;
+        }
+    }
+
+    fn move_cursor_right(&mut self) {
+        if self.cursor.x + 1 < self.dimentions.width {
+            self.cursor.x += 1;
+        }
+    }
+
+    fn move_cursor_to_line_start(&mut self) {
+        self.cursor.x = 0;
+    }
+
+    fn move_cursor_to_line_end(&mut self) {
+        self.cursor.x = self.dimentions.width.saturating_sub(1);
+    }
+
+    fn move_cursor_to_screen_top(&mut self) {
+        for _ in 0..self.dimentions.height {
+            self.move_cursor_up();
+        }
+    }
+
+    fn move_cursor_to_screen_bottom(&mut self) {
+        for _ in 0..self.dimentions.height {
+            self.move_cursor_down();
+        }
+    }
+}
 
 pub struct RawModeOverride;
 
@@ -18,45 +171,31 @@ impl Drop for RawModeOverride {
     }
 }
 
-pub struct Kilo;
+struct Cursor {
+    x: u16,
+    y: u16,
+}
 
-impl Kilo {
-    pub fn new() -> Self {
-        Self
+impl Cursor {
+    fn new(x: u16, y: u16) -> Self {
+        Self { x, y }
     }
+}
 
-    pub fn run(&mut self) -> Result<()> {
-        let _override = RawModeOverride::new()?;
+struct Dimentions {
+    width: u16,
+    height: u16,
+}
 
-        loop {
-            let mut key = None;
-            if event::poll(Duration::from_millis(100))? {
-                if let Event::Key(event) = event::read()? {
-                    key = Some(event);
-                }
-            }
-
-            let key = key;
-            match key {
-                Some(key) => print!("{:?}\r\n", key),
-                None => print!("None\r\n"),
-            }
-
-            if matches!(
-                key,
-                Some(KeyEvent {
-                    code: KeyCode::Char('q'),
-                    modifiers: KeyModifiers::NONE,
-                })
-            ) {
-                break;
-            }
+impl From<(u16, u16)> for Dimentions {
+    fn from((columns, rows): (u16, u16)) -> Self {
+        Self {
+            width: columns,
+            height: rows,
         }
-
-        Ok(())
     }
 }
 
 fn main() -> Result<()> {
-    Kilo::new().run()
+    Kilo::new()?.run()
 }
